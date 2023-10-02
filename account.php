d660cc7d574d3e8722a6795db3f850b3a95d31aa
Soyer13 <99680807+Soyer13@users.noreply.github.com>
2023-10-02 18:41:37 +0200

    Wypisywanie Podstawowych informacji na koncie użytkownika

--- account.php
+++ account.php
@@ -13,7 +13,42 @@
 <body>
     <?php include('header.php'); ?>
     <main>
-        <h1>Konto użytkownika <?php echo $_SESSION['login'];?></h1>
+        <div>
+            <h1>Konto użytkownika <?php echo $_SESSION['login'];?></h1>
+        </div>
+        <div>
+            <?php
+            $login = $_SESSION["login"];
+             $sql = "SELECT * FROM users WHERE login = '$login' ";
+             $dane =  mysqli_query($conn, $sql);
+             if ($dane->num_rows > 0) {
+                 echo "Dane";
+                 $row = mysqli_fetch_assoc($dane);
+                ?>
+                <h3>Imie</h3>
+                <?php
+                 echo $row["first_name"];
+                 ?>
+                 <h3>Nazwisko</h3>
+                 <?php
+                 echo $row["last_name"];
+                 ?>
+                 <h3>E-mail</h3>
+                 <?php
+                 echo $row["email"];
+                 ?>
+                 <h3>Telefon</h3>
+                 <?php
+                 echo $row["phone"];
+                 
+                }
+            else
+            {
+                echo "Bład Bazy Danych";
+            }
+            ?>
+                 
+        </div>
 
         <a href="offersCreator.php"><h2>Nowa Oferta</h2></a>
     </main>
